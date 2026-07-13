2225e88b56fe95c888281f322d1a8bec2c7d0a9b
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-07-13 19:57:49 +0530

    started the function in numpy

---
+++ Numpy/Array_functions/functions.py
@@ -0,0 +1,21 @@
+import numpy as np
+a1=np.arange(12).reshape(3,4)
+a2=np.arange(12,24).reshape(3,4)
+print(a1)
+print(a2)
+
+## max functions
+
+a=np.max(a1)
+print(a)
+
+##minimum number
+
+a=np.min(a2)
+print(a)
+
+##sum
+
+a=np.sum(a2)
+print(a)
+
