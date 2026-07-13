ca9f1eb91435058c9bc3b9be50aaa3347ef85286
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-07-13 20:28:04 +0530

    completed the functions part of the numpy

--- Numpy/Array_functions/functions.py
+++ Numpy/Array_functions/functions.py
@@ -19,3 +19,45 @@ print(a)
 a=np.sum(a2)
 print(a)
 
+##product
+
+a=np.prod(a1)
+print(a)
+
+##axis:- axis mean findng the things in particular row and colunmn
+
+a=np.max(a1,axis=1)
+print(a)
+
+a=np.min(a2,axis=0)
+print(a)
+
+## mean:- calculate the mean of the array
+
+a=np.mean(a1,axis=0)
+print(a)
+a=np.mean(a1)
+print(a)
+
+##median :- calculate the median of the array in particular row and column
+
+a=np.median(a2)
+print(a)
+
+##standard deviation
+
+arr = np.array([10, 20, 30, 40, 50])
+
+std = np.std(arr)
+
+print("Array:", arr)
+print("Standard Deviation:", std)
+
+## variance
+
+arr = np.array([10, 20, 30, 40, 50])
+
+var = np.var(arr)
+
+print("Array:", arr)
+print("Variance:", var)
