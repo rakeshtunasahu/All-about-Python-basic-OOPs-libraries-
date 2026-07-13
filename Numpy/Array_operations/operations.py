49c2de6f4628406132eb868ed944538dfb3bbd03
rakeshtunasahu <rakesh.tuna.sahu@gmail.com>
2026-07-13 20:38:52 +0530

    edited the operations  section

--- Numpy/Array_operations/operations.py
+++ Numpy/Array_operations/operations.py
@@ -1,5 +1,67 @@
 # ## scalar operations:
 # aithmetic:-  +, -, *, /, **.
+# addition
+
+import numpy as np
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([1, 2, 3])
+result = arr1 + arr2
+print(result)
+
+
+## subtraction
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([1, 2, 3])
+result = arr1 - arr
+print(result)
+
+## multiplication 
+
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([2, 3, 4])
+result = arr1 * arr2
+
+print(result)
+
+## division 
+
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([2, 4, 5])
+result = arr1 / arr2
+
+print(result)
+
+## floor division 
+
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([3, 4, 7])
+result = arr1 // arr2
+print(result)
+
+##modules 
+
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([3, 4, 7])
+result = arr1 % arr2
+
+print(result)
+
+## power 
+arr = np.array([2, 3, 4])
+result = arr ** 2
+
+print(result)
+
+## square root 
+arr = np.array([4, 9, 16, 25])
+
+print(np.sqrt(arr)) 
+
+## expotential 
+arr = np.array([1, 2, 3])
+print(np.exp(arr))
+
+##
 # all this can br perform to the matrix of the array.
 
 # ##relational operations:-  <,>,>=,<=,!=,==.
